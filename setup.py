0a312d4916c28cc936339c371092e73f5abe8d44
Jack Walker <jack.walker294@gmail.com>
2016-03-11 14:31:35 +0000

    Add the pydotplus dependency

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ setup(
     name = "Reflect",
     version = "0.0",
     packages = ["reflect"],
-    install_requires = ["imageio", "numpy", "scipy", "cv2", "pygame", "watchdog"]
+    install_requires = ["imageio", "pydotplus", "numpy", "scipy", "cv2", "pygame", "watchdog"]
 )
